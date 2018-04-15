657fab1511ddb3e786a7b7ae7f1f2d6780549f2e
Vadimatorik <deryabkinda@mail.ru>
2018-04-15 21:03:52 +0700

    Оформление скользящего среднего.

--- arithmetic_mean.h
+++ arithmetic_mean.h
@@ -2,44 +2,44 @@
 
 #include <stdint.h>
 
-class arithmetic_mean {
+class arithmeticMean {
 public:
-    arithmetic_mean () {}
+	arithmeticMean () {}
 
-    void post_val ( float val ) {
-        this->val_counter++;
-        if ( val_counter == 1 ) {                    // Фиксируем первый пришедший элемент.
-            this->value = val;
-            return;
-        }
-        this->value = (this->value * (this->val_counter - 1) + val) / ((float)this->val_counter);
-    }
+	void postVal ( float val ) {
+		this->valCounter++;
+		if ( valCounter == 1 ) {					// Фиксируем первый пришедший элемент.
+			this->value = val;
+			return;
+		}
+		this->value = (this->value * (this->valCounter - 1) + val) / ((float)this->valCounter);
+	}
 
-    float get_mean ( void ) {
-        return this->value;
-    }
+	float getMean ( void ) {
+		return this->value;
+	}
 
-    float get_and_reset () {
-        this->val_counter = 0;
-        return this->value;
-    }
+	float getAndReset () {
+		this->valCounter = 0;
+		return this->value;
+	}
 
-    void reset ( void ) {
-        val_counter = 0;
-    }
+	void reset ( void ) {
+		valCounter = 0;
+	}
 
-    void reset_and_post_val ( float val ) {
-        this->reset();
-        this->post_val( val );
-    }
+	void resetAndPostVal ( float val ) {
+		this->reset();
+		this->postVal( val );
+	}
 
-    uint32_t get_counter () {
-        return val_counter;
-    }
+	uint32_t getCounter () {
+		return valCounter;
+	}
 
 private:
-    float               value           = 0;
-    uint32_t            val_counter     = 0;
+	float				value				= 0;
+	uint32_t			valCounter			= 0;
 };
 
 #endif
